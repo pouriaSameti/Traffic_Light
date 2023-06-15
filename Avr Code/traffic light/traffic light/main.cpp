e34a1fed33d98bfb10e456679c03fd9f1bf5d167
pouria <pouria.sameti80@gmail.com>
2023-06-16 02:18:12 +0330

    code initialization with Atmel Studio.

---
+++ Avr Code/traffic light/traffic light/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * traffic light.cpp
+ *
+ * Created: 6/16/2023 2:16:48 AM
+ * Author : acer
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
