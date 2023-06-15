6a0755b3d12f3d99459a743f00af48f51b265882
pouria <pouria.sameti80@gmail.com>
2023-06-16 02:41:06 +0330

    avr code -> conversionA0() was created.

--- Avr Code/traffic light/traffic light/main.cpp
+++ Avr Code/traffic light/traffic light/main.cpp
@@ -6,13 +6,58 @@
  */ 
 
 #include <avr/io.h>
+#define F_CPU 1000000UL
+#include <util/delay.h>
+#include <avr/sfr_defs.h>
 
+
+int conversionA0(){
+	
+	ADMUX = 0x40;
+	ADCSRA |= 0x40;
+	
+	while(ADIF == 0);
+	
+	_delay_us(10);
+	int firstPart = (int)ADCL;
+	int secondPart = (int)ADCH*256;
+	
+	int result = secondPart + firstPart;
+	return (result);
+}
+
+int conversionA1(){
+	
+	ADMUX = 0x41;
+	ADCSRA |= 0x40;
+	
+	while(ADIF == 0);
+	
+	_delay_us(10);
+	int firstPart = (int)ADCL;
+	int secondPart = (int)ADCH*256;
+	
+	int result = secondPart + firstPart;
+	return (result);
+}
 
 int main(void)
 {
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
-}
+	
+	DDRC = 0x01;
+	
+	DDRA = 0x00;
+	ADCSRA = 0x87;
+	ADMUX = 0x40;
+	
+	while (1)
+	{
 
+		int value = conversionA0();
+		
+		if (value > 512)
+			PORTC |= 0x01;
+		else
+			PORTC = 0x00;
+	}
+}
