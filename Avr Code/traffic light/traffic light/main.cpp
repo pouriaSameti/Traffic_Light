4bf2a6e9867ec9f48109c1f08af228c8fc0923a7
pouria <pouria.sameti80@gmail.com>
2023-06-27 12:33:16 +0330

    avr code -> waitFor() was implemented.

--- Avr Code/traffic light/traffic light/main.cpp
+++ Avr Code/traffic light/traffic light/main.cpp
@@ -35,40 +35,10 @@ int conversionA1(){
 
 void waitFor(int secs) {
 	for (int i=0; i<secs; ++i)
-			_delay_ms(1000);
+	_delay_ms(1000);
 }
 
 int main(void)
 {
 	
-	DDRC = 0xff;
-	
-	DDRA = 0x00;
-	ADCSRA = 0x87;
-	ADMUX = 0x40;
-	
-	int delayHorizental, midDelay, delayVertical;
-
-	while (1)
-	{
-
-		int value = conversionA0();
-		int value2 = conversionA1();
-		
-		delayHorizental = value * 0.05;
-		midDelay = value * 0.008;
-		delayVertical = (value * 0.05) * (512 / value2);
-			
-		PORTC = 0x24;
-		waitFor(delayHorizental);
-		
-		PORTC = 0x22;
-		waitFor(midDelay);
-		
-		PORTC = 0x81;
-		waitFor(delayVertical);
-		
-		PORTC = 0x41;
-		waitFor(midDelay);	
-	}
 }
